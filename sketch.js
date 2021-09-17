659956fcaf749c383c8d73f6153f23b4e448f107
Rmnitin12 <68161753+Rmnitin12@users.noreply.github.com>
2021-09-16 18:51:43 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+var box 
+function setup() {
+  createCanvas(400,400);
+  box = createSprite(100,100,20,20)
+}
+
+function draw() 
+{
+  background(30);
+  if(keyDown(RIGHT_ARROW)){
+    box.x += 5
+  }
+
+  if(keyDown(LEFT_ARROW)){
+    box.x -= 5
+  }
+  if(keyDown(DOWN_ARROW)){
+    box.y += 5
+  }
+  if(keyDown(UP_ARROW)){
+    box.y -= 5
+  }
+
+drawSprites()
+}
+
+
+
+
